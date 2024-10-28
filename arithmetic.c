7e3611882ee291624bb26bfebf78a1fafefc96f7
Anamika Singh Raj <158223336+Anamikasinghraj@users.noreply.github.com>
2024-10-29 02:49:28 +0530

    Update arithmetic.c

--- arithmetic.c
+++ arithmetic.c
@@ -1,5 +1,18 @@
-// Addition Program
+//Addition, Subtraction, Multiplicatin, Division program
+
 #include<stdio.h>
 int main()
 {
-int num1, num2, sum;
+    int num1, num2, diff,Prod,Div,Add;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &num1, &num2);
+    diff = num1 - num2;
+    Add =num1+num2;
+    Prod=num1*num2;
+    Div=num1/num2;
+    printf("Difference: %d\n",diff);
+    printf("Add:%d\n",Add);
+    printf("Prod:%d\n",Prod);
+    printf("Div:%d\n",Div);
+    return 0;
+}
